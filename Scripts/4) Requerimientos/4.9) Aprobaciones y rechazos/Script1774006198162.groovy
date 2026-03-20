2fde7b9cb44dd5d90c5d56e4090c8dfae47e2f39
Omar Eduardo Brondo <omar.brondo@intiza.com>
2026-03-20 09:07:57 -0300

    primera version de aprobaciones pendientes

--- Scripts/4) Requerimientos/4.9) Aprobaciones y rechazos/Script1774006198162.groovy
+++ Scripts/4) Requerimientos/4.9) Aprobaciones y rechazos/Script1774006198162.groovy
@@ -1,19 +1,70 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.testobject.ConditionType
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import com.kms.katalon.core.model.FailureHandling
+import org.openqa.selenium.WebElement
 
+// ===============================
+// 0) LOGIN
+// ===============================
+WebUI.callTestCase(findTestCase('1) Logins/1.1) Login Usuario Interno'), [:], FailureHandling.STOP_ON_FAILURE)
+
+
+import org.openqa.selenium.interactions.Actions
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+// ===============================
+// 1) MOUSEOVER SOBRE EL MENÚ REQUERIMIENTOS
+// ===============================
+TestObject menuReqNav = new TestObject('menuReqNav')
+menuReqNav.addProperty(
+    "xpath",
+    ConditionType.EQUALS,
+    "//li[contains(@class,'nav-item') and contains(@class,'dropdown')]//a[contains(@class,'nav-link') and .//span[normalize-space()='Requerimientos']]"
+)
+
+WebUI.waitForElementVisible(menuReqNav, 10)
+WebUI.mouseOver(menuReqNav)
+WebUI.delay(1)
+
+
+// ===============================
+// 2) FORZAR APERTURA DEL DROPDOWN (MISMO QUE ANTES)
+// ===============================
+String jsOpenReqNav = """
+document.querySelectorAll("li.nav-item.dropdown.dropend").forEach(function(li){
+    var span = li.querySelector("span.w-md.text-truncate");
+    if(span && span.innerText.trim() === "Requerimientos"){
+        var menu = li.querySelector(".dropdown-menu");
+        if(menu){
+            menu.classList.add("show");
+            menu.style.display = "block";
+        }
+    }
+});
+"""
+WebUI.executeJavaScript(jsOpenReqNav, null)
+WebUI.delay(1)
+
+
+// ===============================
+// 3) CLICK EN "Aprobaciones pendientes" VIA JS (IGNORANDO BADGE)
+// ===============================
+TestObject opcionAprobPend = new TestObject('opcionAprobPend')
+opcionAprobPend.addProperty(
+    "xpath",
+    ConditionType.EQUALS,
+    "//div[contains(@class,'dropdown-menu')]//a[contains(@class,'dropdown-item') and contains(normalize-space(.),'Aprobaciones pendientes')]"
+)
+
+WebUI.waitForElementVisible(opcionAprobPend, 10)
+
+// CLICK REAL VIA JAVASCRIPT (evita overlay invisible)
+WebUI.executeJavaScript(
+    "arguments[0].click();",
+    Arrays.asList(WebUI.findWebElement(opcionAprobPend))
+)
+
+WebUI.comment("✔ Se ingresó correctamente a Aprobaciones pendientes")
